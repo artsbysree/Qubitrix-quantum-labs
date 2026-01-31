5cfcb4d03b508046be1cf46afc2c503ea574782e
Sree divya <160315585+artsbysree@users.noreply.github.com>
2026-02-01 04:36:49 +0530

    Refactor to use createRoot from react-dom

--- client/main.tsx
+++ client/main.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./global.css";
 
-const root = document.getElementById("root");
-
-if (!root) {
-  throw new Error("Root element not found");
-}
-
-ReactDOM.createRoot(root).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
